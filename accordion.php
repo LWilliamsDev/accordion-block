f9e8979a981f242202ed8f5a33f775af9f76b600
Lisa Williams <lisawilliams@admins-MacBook-Pro.local>
2022-09-25 23:35:37 -0500

    Added description and corrected author

--- accordion.php
+++ accordion.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Accordion
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Description:       Sample accordion block
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Lisa Williams
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       accordion
